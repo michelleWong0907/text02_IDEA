105619167410dd315167c4baabb6cd4de66977d8
1490555159 <1490555159@qq.com>
2020-08-25 14:25:32 +0800

    第一次提交

---
+++ src/main/java/cn/java/GitDay0101Application.java
@@ -0,0 +1,13 @@
+package cn.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDay0101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDay0101Application.class, args);
+    }
+
+}
---
+++ src/main/java/cn/java/web/GitController.java
@@ -0,0 +1,19 @@
+package cn.java.web;
+/*
+ *TODO
+ *@author    wh
+ *@date  2020/8/25/0025    14:19
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitController {
+
+    @GetMapping("init")
+    public String init(){
+        return "hello git!";
+
+    }
+}
