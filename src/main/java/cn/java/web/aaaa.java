50939953dc514f3a32eec9bea0cca32b23de1130
1490555159 <1490555159@qq.com>
2020-08-25 15:14:12 +0800

    新增文件

---
+++ src/main/java/cn/java/web/aaaa.java
@@ -0,0 +1,12 @@
+package cn.java.web;
+/*
+ *TODO
+ *@author    wh
+ *@date  2020/8/25/0025 15:00
+ */
+
+public class aaaa {
+    public static void main(String[] args) {
+        System.out.println(123456789);
+    }
+}
